29c365db44ec9fc1d11faf1929555a9363c25fcb
Deyan Hadzhiev <deyan.z.hadzhiev@gmail.com>
2014-02-06 21:48:17 +0200

    Project: Updates tests for splines and for utilites

--- project/spec/spec.rb
+++ project/spec/spec.rb
@@ -1,5 +1,35 @@
 require 'Matrix'
 
+describe "Utilities" do
+  it "calculate factorials (small)" do
+    Combinatorics.factorial(5).should eq 120
+  end
+
+  it "calculate factorials (big)" do
+    Combinatorics.factorial(42).should eq 1405006117752879898543142606244511569936384000000000
+  end
+
+  it "calculate binomial (small)" do
+    Combinatorics.binomial(5,2).should eq 10
+  end
+
+  it "calculate binomial (big)" do
+    Combinatorics.binomial(73, 23).should eq 5685248339583664176
+  end
+
+  it "calculate bernstein values 1" do
+    Combinatorics.bernstein(0, 0, 1).should eq 1.0
+  end
+
+  it "calculate bernstein values 2" do
+    Combinatorics.bernstein(1, 0, 0.6).should eq 0.4
+  end
+
+  it "calculate bernstein values 3" do
+    Combinatorics.bernstein(2, 1, 0.6).should eq 0.48
+  end
+end
+
 describe "Polygon" do
   VECTOR_ARRAY = [
       Vector[0, 0, 0],
@@ -57,6 +87,16 @@ describe "Spline" do
       Vector[0, 8, 0],
     ].freeze
 
+  COTROL_POINTS_EXT = [
+      Vector[0, 0, 0],
+      Vector[4, 0, 0],
+      Vector[0, 4, 0],
+      Vector[4, 4, 0],
+      Vector[0, 4, 0],
+      Vector[4, 8, 0],
+      Vector[0, 8, 0],
+    ].freeze
+
   it "creates splines properly from arrays" do
     poly = Polygon.new CONTROL_POINTS
     spline = Spline.new CONTROL_POINTS, 3
@@ -69,13 +109,28 @@ describe "Spline" do
     spline.control_polygon.points.should eq poly.points
   end
 
-  it "calculates spline points properly" do
+  it "calculates spline points properly (degree 3)" do
     spline = Spline.new CONTROL_POINTS, 3
-    spline.get_precise_points(5).should eq [
+    spline.get_precise_points(4).should eq [
       Vector[0, 0, 0],
       Vector[1.5, 2, 0],
       Vector[2, 4, 0],
       Vector[1.5, 6, 0],
+      Vector[0, 8, 0],
+    ]
+  end
+
+  it "calculates spline points properly (degree 4)" do
+    spline = Spline.new COTROL_POINTS_EXT, 4
+    spline.get_precise_points(4).should eq [
+      Vector[0, 0, 0],
+      Vector[1.75, 0.625, 0.0],
+      Vector[2.0, 2.0, 0.0],
+      Vector[2.25, 3.375, 0.0],
+      Vector[4.0, 4.0, 0.0],
+      Vector[2.25, 4.625, 0.0],
+      Vector[2.0, 6.0, 0.0],
+      Vector[1.75, 7.375, 0.0],
       Vector[0, 8, 0],
     ]
   end
