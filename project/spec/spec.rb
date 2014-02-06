68253ee46de5c1236d77cf660e8fcdb625ea1add
Deyan Hadzhiev <deyan.z.hadzhiev@gmail.com>
2014-02-06 10:26:49 +0200

    Project: Includes the spline tests to the spec.rb

--- project/spec/spec.rb
+++ project/spec/spec.rb
@@ -1,6 +1,6 @@
 require 'Matrix'
 
-describe "Polygon tests" do
+describe "Polygon" do
   VECTOR_ARRAY = [
       Vector[0, 0, 0],
       Vector[1.5, 2, 0],
@@ -50,14 +50,42 @@ describe "Polygon tests" do
   end
 end
 
-describe "Spline tests" do
+describe "Spline" do
+  CONTROL_POINTS = [
+      Vector[0, 0, 0],
+      Vector[4, 4, 0],
+      Vector[0, 8, 0],
+    ].freeze
+
+  it "creates splines properly from arrays" do
+    poly = Polygon.new CONTROL_POINTS
+    spline = Spline.new CONTROL_POINTS, 3
+    spline.control_polygon.points.should eq poly.points
+  end
+
+  it "creates splines properly from polygons" do
+    poly = Polygon.new CONTROL_POINTS
+    spline = Spline.new poly, 3
+    spline.control_polygon.points.should eq poly.points
+  end
+
+  it "calculates spline points properly" do
+    spline = Spline.new CONTROL_POINTS, 3
+    spline.get_precise_points(5).should eq [
+      Vector[0, 0, 0],
+      Vector[1.5, 2, 0],
+      Vector[2, 4, 0],
+      Vector[1.5, 6, 0],
+      Vector[0, 8, 0],
+    ]
+  end
 end
 
-describe "Polygon Resolver tests" do
+describe "Polygon Resolver" do
 end
 
-describe "Mesh tests" do
+describe "Mesh" do
 end
 
-describe "Mesh Exporter tests" do
+describe "Mesh Exporter" do
 end
