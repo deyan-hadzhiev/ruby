88e38994f086ef6c8d4fe087c3cb4b2cc17623ff
Deyan Hadzhiev <deyan.z.hadzhiev@gmail.com>
2014-02-07 15:11:31 +0200

    Project: Implements Transformations utilites module for obtaining rotational matrices. Monkey patches the == operator of Floats to work with Epsilon value.

--- project/src/core/utils.rb
+++ project/src/core/utils.rb
@@ -1,5 +1,13 @@
+require 'Matrix'
+
 module Constants
   EPS = 1e-6.freeze
+end
+
+class Float
+  def ==(rhs)
+    (self - rhs).abs < Constants::EPS
+  end
 end
 
 module Combinatorics
@@ -18,6 +26,29 @@ module Combinatorics
 
     def bernstein(degree, index, t)
       binomial(degree, index) * (t ** index) * ((1 - t) ** (degree - index))
+    end
+  end
+end
+
+module Transformations
+  class << self
+    def get_rotational_matrix(axis, angle)
+      case axis
+        when :x
+          Matrix[ [1, 0, 0],
+                  [0, Math.cos(angle), - Math.sin(angle)],
+                  [0, Math.sin(angle), Math.cos(angle)]]
+        when :y
+          Matrix[ [Math.cos(angle), 0, Math.sin(angle)],
+                  [0, 1, 0],
+                  [- Math.sin(angle), 0, Math.cos(angle)]]
+        when :z
+          Matrix[ [Math.cos(angle), - Math.sin(angle), 0],
+                  [Math.sin(angle), Math.cos(angle), 0],
+                  [0, 0, 1]]
+        else
+          Matrix.identity(3)
+      end
     end
   end
 end
